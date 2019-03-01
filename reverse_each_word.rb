afa4bb2317615b0d2dacae487792b8539e5421f7
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-01 06:34:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   reverse_string = []
   reverse = string.split
   reverse.each do |word|
-    reverse_string << word.split
+    reverse_string << word.reverse
   end
   return reverse_string
 end
